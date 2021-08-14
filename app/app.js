ed03838517766fb671b6cb7ede43a27d887f368b
Luciano Ibarra 182775 <luciano39340268@gmail.com>
2021-08-14 18:08:53 -0300

    Create app.js

---
+++ app/app.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal('.content-img', { delay: 200 });
+ScrollReveal().reveal('.services__title', { delay: 200 });
+ScrollReveal().reveal('.services__content__card', { delay: 500 });
+ScrollReveal().reveal('.projects__title', { delay: 200 });
+ScrollReveal().reveal('.projects__content__card', { delay: 500 });
+ScrollReveal().reveal('.about__text-content', { delay: 300 });
+ScrollReveal().reveal('.about__img-content', { delay: 700 });
+ScrollReveal().reveal('.content', { delay: 300 });
+ScrollReveal().reveal('.footer', { delay: 300 });
+
