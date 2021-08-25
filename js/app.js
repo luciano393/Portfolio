04a80cab57c29be2d42d8287021dbba1e78a0e37
Luciano Ibarra 182775 <luciano39340268@gmail.com>
2021-08-25 00:22:52 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 import hamburgerMenu from "./hamburger-menu.js";
 
 const d = document;
+var scroll = new SmoothScroll('a[href*="#"]');
 
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu('.btn-menu','.menu','.menu a');
 })
+	
