e6268452401086c9e077cab2e742e2a1aed7675e
luciano393 <83837558+luciano393@users.noreply.github.com>
2021-09-07 13:07:23 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
+import contactFormValidations from "./formulario.js";
 import hamburgerMenu from "./hamburger-menu.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu('.btn-menu','.menu','.menu a');
+    contactFormValidations();
 })
 	
