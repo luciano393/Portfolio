7d7bfc08076c479e55ba67f35af43108655f90a4
luciano393 <83837558+luciano393@users.noreply.github.com>
2021-09-07 16:18:47 -0300

    Update formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -31,7 +31,19 @@ export default function contactFormValidations() {
     })
 
     d.addEventListener("submit", (e) => {
-        e.preventDefault();
-        alert("Sending form");
-    })
+        //e.preventDefault();
+
+        const $loader = d.querySelector(".contact-form-loader"),
+            $response = d.querySelector('.contact-form-response');
+
+        $loader.classList.remove('none');
+
+        setTimeout(() => {
+            $loader.classList.add('none');
+            $response.classList.remove('none');
+            $form.reset();
+
+            setTimeout(() => $response.classList.add('none'),3000);
+        }, 3000);
+    });
 }
