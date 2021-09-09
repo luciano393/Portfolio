3ce421d52b2bdb2c3b0b1cb29197483e567f9461
luciano393 <83837558+luciano393@users.noreply.github.com>
2021-09-09 07:44:42 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,9 +8,3 @@ d.addEventListener("DOMContentLoaded", (e) => {
     contactFormValidations();
 })
 
-
-ScrollReveal().reveal('#portfolio', {delay: 700});
-ScrollReveal().reveal('#skills', { delay: 700 });
-ScrollReveal().reveal('#about', { delay: 700 });
-ScrollReveal().reveal('#contact', { delay: 700 });
-ScrollReveal().reveal('footer', { delay: 700 });
